0de596b9f38cef7da079cbdb1cf9b61e9ee2258e
luwbe1 <luwbe1@naver.com>
2025-04-21 23:15:16 +0900

    Feature: useCoupon, useProduct 구현

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,5 +2,11 @@ import { useState } from "react";
 import { Coupon } from "../../types.ts";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: [], addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState<Coupon[]>(initialCoupons);
+
+  const addCoupon = (coupon: Coupon) => {
+    setCoupons((prevCoupons) => [...prevCoupons, coupon]);
+  };
+
+  return { coupons, addCoupon };
 };
--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,27 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts); // 초기화
+
+  // 제품을 업데이트할 수 있다.
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((product) =>
+        product.id === updatedProduct.id
+          ? { ...product, name: "Updated Product" }
+          : product
+      )
+    );
+  };
+
+  // 새로운 제품을 추가할 수 있다.
+  const addProduct = (newProduct: Product) => {
+    setProducts((prevProducts) => [...prevProducts, newProduct]);
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
